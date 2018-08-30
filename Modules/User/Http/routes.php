aa455cdf3d2671f20f591a1a68722712d97c4ed0
Tahsin <tahsinahmed2012@gmail.com>
2018-08-30 10:35:09 +0600

    role_permission backend code

--- Modules/User/Http/routes.php
+++ Modules/User/Http/routes.php
@@ -2,8 +2,13 @@
 
 Route::group(['middleware' => 'api', 'prefix' => 'user', 'namespace' => 'Modules\User\Http\Controllers'], function()
 {
+	//Users
     Route::get('', 'UserController@index');
     Route::post('','UserController@store')->middleware(['jwt.auth', 'permission:user-store']);
+
+    //Permission
+    Route::post('/permission/create', 'PermissionController@store')->middleware(['jwt.auth']);
+    Route::get('/permission', 'PermissionController@index')->middleware(['jwt.auth']);
 
     Route::post('/login', 'AuthenticationController@login');
     Route::get('/get_auth_user', 'AuthenticationController@getAuthenticatedUser')->middleware('jwt.auth');
